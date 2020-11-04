475f77104e62e442feee3ad6dc303d3fdbd55fbf
Faiz <faiz.anwar4749@gmail.com>
2020-11-04 08:07:51 +0530

    [Faiz] Add: Welcome message

---
+++ HotelReservationSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HotelReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hotel Reservation System!");
+        }
+    }
+}
