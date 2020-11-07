06116fbf5e3ab031cbb1007d912eaf71dc49a9af
Faiz <faiz.anwar4749@gmail.com>
2020-11-07 08:14:55 +0530

    [Faiz] Add: Adding Hotel to the hotel reservation system

---
+++ HotelReservationSystem/HotelReservationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace HotelReservationSystem
+{
+    class HotelReservationException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_HOTEL_TYPE
+        }
+        ExceptionType type;
+        public HotelReservationException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ HotelReservationSystem/HotelType.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace HotelReservationSystem
+{
+    public enum HotelType { LAKEWOOD, BRIDGEWOOD, RIDGEWOOD }
+}
